b464ea20f1171d21c60e8c50bd8f416f2b62ee71
Douglas <douglaszaltron@gmail.com>
2018-08-27 19:45:53 -0300

    Support cordova 6

---
+++ README.md
@@ -0,0 +1,30 @@
+# :art: cordova-plugin-window-background
+
+[![Latest Stable Version](https://img.shields.io/npm/v/cordova-plugin-window-background.svg)](https://www.npmjs.com/package/cordova-plugin-window-background)
+[![Total Downloads](https://img.shields.io/npm/dt/cordova-plugin-window-background.svg)](https://npm-stat.com/charts.html?package=cordova-plugin-window-background)
+
+This is a simple plugin for Android which will set the window background color for you app's MainActivity to a color of your choice.
+This allows you to avoid the standard dark grey background which will flash for a second before your splash screen appears. Set the color to the same background as your splash screen for a smoother start-up effect.
+
+## Installation
+
+You'll need to set your desired window background color while installing the plugin by supplying the `WINDOW_BACKGROUND_COLOR` variable. Set it to whatever you want, preferrably the same color as your splash screen.
+
+    cordova plugin add cordova-plugin-window-background --variable WINDOW_BACKGROUND_COLOR=#ffffff --save
+	
+Or install directly from this repo:
+
+    cordova plugin add https://github.com/douglaszaltron/cordova-plugin-window-background.git --variable WINDOW_BACKGROUND_COLOR=#ffffff --save
+
+	
+## Supported Platforms
+
+- Android (cordova-android 6.0.0+)
+
+## Configuration
+
+A custom `WINDOW_BACKGROUND_COLOR` variable will be added to your app's config.xml after installation with the value you selected (provided you included the `--save` flag).
+
+<plugin name="cordova-plugin-window-background">
+    <variable name="WINDOW_BACKGROUND_COLOR" value="#ffffff" />
+</plugin>
---
+++ scripts/apply-changes.js
@@ -0,0 +1,95 @@
+const PLUGIN_NAME = "cordova-plugin-window-background";
+
+const V6 = "cordova-android@6";
+const V7 = "cordova-android@7";
+
+var FILE_PATHS = {};
+
+FILE_PATHS[V6] = {
+    "android.manifest": "platforms/android/AndroidManifest.xml",
+    "android.styles": "platforms/android/res/values/cordova-plugin-window-background-styles.xml"
+};
+
+FILE_PATHS[V7] = {
+    "android.manifest": "platforms/android/app/src/main/AndroidManifest.xml",
+    "android.styles": "platforms/android/app/src/main/res/values/cordova-plugin-window-background-styles.xml"
+};
+
+var deferral, fs, path, parser, platformVersion;
+
+function log(message) {
+    console.log(PLUGIN_NAME + ": " + message);
+}
+
+function onError(error) {
+    log("ERROR: " + error);
+    deferral.resolve();
+}
+
+function getCordovaAndroidVersion() {
+    var cordovaVersion = require(path.resolve(process.cwd(), 'platforms/android/cordova/version'));
+    return parseInt(cordovaVersion.version) === 7 ? V7 : V6;
+}
+
+
+function run() {
+
+    try {
+        fs = require('fs');
+        path = require('path');
+        parser = require('xml2js');
+    } catch (e) {
+        throw ("Failed to load dependencies. If using cordova@6 CLI, ensure this plugin is installed with the --fetch option: " + e.toString());
+    }
+
+    platformVersion = getCordovaAndroidVersion();
+    log("Android platform: " + platformVersion);
+
+    var data = fs.readFileSync(path.resolve(process.cwd(), 'config.xml'));
+
+    parser.parseString(data, attempt(function (err, result) {
+
+        if (err) throw err;
+
+        var color, plugins = result.widget.plugin;
+
+        for (var n = 0, len = plugins.length; n < len; n++) {
+            var plugin = plugins[n];
+            if (plugin.$.name === PLUGIN_NAME && plugin.variable && plugin.variable.length > 0) {
+                color = plugin.variable.pop().$.value;
+                break;
+            }
+        }
+
+        if (color) {
+            var manifestPath = path.resolve(process.cwd(), FILE_PATHS[platformVersion]["android.manifest"]);
+            var contents = fs.readFileSync(manifestPath).toString();
+            fs.writeFileSync(manifestPath, contents.replace(/(android:theme="@[^=]+")()/g, 'android:theme="@style/AppTheme"'), 'utf8');
+
+            var stylesPath = path.resolve(process.cwd(), FILE_PATHS[platformVersion]["android.styles"]);
+            fs.writeFileSync(stylesPath, '<resources><style name="AppTheme" parent="@android:style/Theme.DeviceDefault.NoActionBar"><item name="android:windowBackground">@color/windowBackgroundColor</item></style><color name="windowBackgroundColor">' + color + '</color></resources>');
+            
+        } else {
+            log("No custom color found in config.xml - using plugin default");
+        }
+
+        deferral.resolve();
+
+    }));
+}
+
+function attempt(fn) {
+    return function () {
+        try {
+            fn.apply(this, arguments);
+        } catch (e) {
+            onError("EXCEPTION: " + e.toString());
+        }
+    }
+}
+
+module.exports = function (ctx) {
+    deferral = ctx.requireCordovaModule('q').defer();
+    attempt(run)();
+    return deferral.promise;
+};
